d0d6933b5d29102dac64b1ffe67ff1df4e2f1e68
SHEIKH SAJIB <sheikhsajib99@gmail.com>
2023-02-28 07:56:23 +0600

    Firebase Connect Problem

--- app/src/main/java/com/example/jobcombat/MainActivity.java
+++ app/src/main/java/com/example/jobcombat/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.jobcombat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.jobcombat.databinding.ActivityMainBinding;
 
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
 
         ArrayList<CategoryModel> categories = new ArrayList<>();
         categories.add(new CategoryModel("", "Bangla" , "https://cdn2.iconfinder.com/data/icons/ad-network-icon-line-fill-set-1/100/bangla-512.png"));
@@ -34,5 +40,22 @@ public class MainActivity extends AppCompatActivity {
         CategoryAdapter adapter = new CategoryAdapter(this, categories);
         binding.categoryList.setLayoutManager(new GridLayoutManager(this, 2));
         binding.categoryList.setAdapter(adapter);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.wallet){
+            Toast.makeText(this, "Wallet is clicked.", Toast.LENGTH_SHORT ).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/jobcombat/SignupActivity.java
+++ app/src/main/java/com/example/jobcombat/SignupActivity.java
@@ -1,14 +1,55 @@
 package com.example.jobcombat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.jobcombat.databinding.ActivitySignupBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
+
+    ActivitySignupBinding binding;
+    FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+
+    binding.signUpSubmitBtn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String email, pass;
+
+            email = binding.emailBox.getText().toString();
+            pass = binding.passwordBox.getText().toString();
+
+        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(SignupActivity.this, "Success", Toast.LENGTH_SHORT ).show();
+                }else {
+                    Toast.makeText(SignupActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        }
+    });
+
+
+
+
     }
 }
